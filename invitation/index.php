7de9a072f04ff6a5a32bfd2020d1e38d311c3da3
DavidM <jdm@esokia-webagency.com>
2024-03-19 14:49:33 +0400

    Create Form

--- invitation/index.php
+++ invitation/index.php
@@ -6,6 +6,12 @@
 <body>
     <h1>You are invited TO MY BIRTHDAY!</h1>
 
+    <form action="" method="GET">
+        <input type="input" name="name" placeholder="Name">
+        <input type="input" name="guests_count" placeholder="Guest Count">
+        <input type="input" name="phone_number" placeholder="Phone Number">
+        <input type="submit" value="Please Register Me!">
+    </form>
 
 </body>
 </html>
