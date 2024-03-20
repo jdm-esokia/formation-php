95447f10881044ccbd35da06ca395198d483043c
DavidM <jdm@esokia-webagency.com>
2024-03-20 11:29:52 +0400

    debuggin

---
+++ debugging/index.php
@@ -0,0 +1,12 @@
+<?php
+
+$a = "Macaroni Sevrette";
+var_dump($a); //will display everything contained in the $a variable
+echo "<hr>";
+
+$b = 104.124;
+var_dump($b); //will display everything contained in the variable $b
+echo "<hr>";
+
+$c = ['and', 3, 3.1429, 'fitter'];
+var_dump($c); //will display everything contained in the variable $c
