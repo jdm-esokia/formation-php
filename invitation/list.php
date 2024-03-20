368dc957efcc30eff157238ac5b8129d80f1e436
DavidM <jdm@esokia-webagency.com>
2024-03-20 10:56:10 +0400

    Moved Mysql Connection

--- invitation/list.php
+++ invitation/list.php
@@ -8,7 +8,7 @@
 <body>
     <?php 
     //display all invitations
-    include '../mysql_connection.php';
+    include '../mysql_connection/mysql_connection.php';
     $sql = "SELECT * FROM birthday_invitations";
     $result = $conn->query($sql);
 
