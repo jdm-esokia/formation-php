67b90da94d1a84c9bbb6b60080c54d7e2e476dc0
DavidM <jdm@esokia-webagency.com>
2024-04-30 08:30:58 +0400

    Tombola Update

--- tombola/index.php
+++ tombola/index.php
@@ -4,9 +4,45 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tombola!</title>
+    <style>
+        body {
+            text-align: center;
+            font-family: Arial, sans-serif;
+        }
+        img {
+            width: 200px;
+            height: 200px;
+        }
+        form {
+            margin-top: 20px;
+        }
+        input {
+            padding: 10px;
+            font-size: 16px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+        }
+        button {
+            padding: 10px 20px;
+            font-size: 16px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            background-color: #f3f3f3;
+            cursor: pointer;
+        }
+        button:hover {
+            background-color: #e9e9e9;
+        }
+        h2 {
+            margin-top: 20px;
+            color: #333;
+        }
+    </style>
 </head>
 <body>
-    <h1>Please enter your Tombola Ticket Number!</h1>
+    <h1> Welcome to Tombola Ticket Checker!</h1>
+    <img src="tombola.png"/>
+    <h3>Please enter your Tombola Ticket Number</h3>
     <form action="" method="GET">
         <input type="text" name="ticket_number" placeholder="000">
         <button type="submit">Verify My Ticket</button>
@@ -21,7 +57,7 @@
             $ticket = $_GET['ticket_number'];
 
             if(in_array($ticket, $winningNumbers)){
-                echo "<h2>Congratulations! You have won the Tombola!</h2>";
+                echo "<h1 style='color:blue'>Congratulations! You have won the Tombola!</h1>";
             } else {
                 echo "<h2>Sorry, you have not won the Tombola!</h3>";
             }
